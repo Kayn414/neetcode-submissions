5c0c558bfaccd4c68e7b056726ef70fd1f9df21e
Kayn414 <148278271+Kayn414@users.noreply.github.com>
2026-08-06 14:18:27 -0500

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        closedSym = {")":"(", "}" : "{", "]" : "["}
+        stack = []
+
+        for c in s:
+            if c in closedSym:
+                if stack and stack[-1] == closedSym[c]:
+                    stack.pop()
+                else:
+                    return False
+            
+            else:
+                stack.append(c)
+        
+        return len(stack) == 0
+        
